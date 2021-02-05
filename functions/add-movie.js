d69ad7303aa9c660dc8a5497b464b55705656c0e
Steven Tessler <stevenkabc@redwood6.com>
2021-02-04 22:21:52 -0500

    feat: add-movie.js function and admin.js mutation query

---
+++ functions/add-movie.js
@@ -0,0 +1,24 @@
+const {query} = require('./util/hasura')
+ 
+exports.handler = async (event) => {
+  const {id, title, tagline, poster} = JSON.parse(event.body);
+
+  const result = await query({
+    query: `
+     mutation MyMutation($title: String!, $id: String!, $poster: String!, $tagline: String!) {
+  insert_movies_one(object: {id: $id, poster: $poster, tagline: $tagline, title: $title}) {
+    id
+    poster
+    tagline
+    title
+  }
+}
+
+    `,
+    variables: { id, title,tagline, poster}
+  })
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  };
+}
